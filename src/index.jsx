61c0bacd54dc14587366c93489a7f084dbae4868
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-07 16:20:50 -0800

    add Slider function

--- src/index.jsx
+++ src/index.jsx
@@ -5,27 +5,21 @@ import "./styles.css";
 function Slider({ onChange, min, max }) {
   const [value, setValue] = useState(1);
 
-  function reducer(state, action) {
-    if (action === "increment") {
-      return state + 1;
-    } else if (action === "decrement") {
-      return state - 1;
-    } else if (action === "reset") {
-      return 0;
-    } else {
-      throw new Error();
-    }
-  }
-
-  function Counter() {
-    const [count, dispatch] = useReducer(reducer, 0);
 
     return (
       <>
-        <h1>{count}</h1>
-        <button onClick={() => dispatch("increment")}>+</button>
-        <button onClick={() => dispatch("decrement")}>-</button>
-        <button onClick={() => dispatch("reset")}>Reset</button>
+        {value}
+        <input 
+        type="range"
+        min={min}
+        max={max}
+        value={value}
+        onChange={(e) => {
+          const value = Number(e.target.value)
+          onChange(value)
+          setValue(value)
+        }}
+        />
       </>
     );
   }
