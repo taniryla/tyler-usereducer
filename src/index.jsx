682354693a08580e0fc822c5ad19b45da668626c
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-07 19:22:16 -0800

    added slider

--- src/index.jsx
+++ src/index.jsx
@@ -6,20 +6,25 @@ function Slider({ onChange, min, max }) {
   const [value, setValue] = useState(1);
 
   function reducer(state, action) {
-    if (action === "increment") {
+    if (action.type === "increment") {
       return {
-        count: state.count + 1,
-        step: state.step 
-      } 
-    } else if (action === "decrement") {
-      return {
-        count: state.count - 1,
+        count: state.count + state.step,
         step: state.step
-      }
-    } else if (action === "reset") {
+      };
+    } else if (action.type === "decrement") {
+      return {
+        count: state.count - state.step,
+        step: state.step
+      };
+    } else if (action.type === "reset") {
       return {
         count: 0,
         step: state.step
+      };
+    } else if (action.type === "updateStep) {
+      return {
+        count: state.count,
+        step: action.step
       }
     } else {
       throw new Error();
@@ -27,30 +32,31 @@ function Slider({ onChange, min, max }) {
   }
 
   function Counter() {
-    const [state, dispatch] = useReducer(
-      reducer,
-      { count: 0, step: 1}
-      );
+    const [state, dispatch] = useReducer(reducer, { count: 0, step: 1 });
 
     return (
       <>
-        <Slider 
+        <Slider
           min={1}
           max={10}
-          onChange={() => ({})}
+          onChange={(value) =>
+            dispatch({
+              type: "updateStep",
+              step: value
+            })
+          }
         />
-        <hr>
-        {value}
-        <input 
-        type="range"
-        min={min}
-        max={max}
-        value={value}
-        onChange={(e) => {
-          const value = Number(e.target.value)
-          onChange(value)
-          setValue(value)
-        }}
+        <hr></hr>s{value}
+        <input
+          type="range"
+          min={min}
+          max={max}
+          value={value}
+          onChange={(e) => {
+            const value = Number(e.target.value);
+            onChange(value);
+            setValue(value);
+          }}
         />
         <h1>{state.count}</h1>
         <button onClick={() => dispatch("increment")}>+</button>
